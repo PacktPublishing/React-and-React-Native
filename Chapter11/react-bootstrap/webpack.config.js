187b85ffe454ec4e02bec8f663ceaa9e53dd55f1
Adam Boduch <adam.boduch@gmail.com>
2017-06-05 12:44:00 -0400

    Fixed invalid Webpack config. #6

--- Chapter11/react-bootstrap/webpack.config.js
+++ Chapter11/react-bootstrap/webpack.config.js
@@ -14,10 +14,31 @@ module.exports = {
         loaders: ['babel-loader'],
       },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
-			{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-			{ test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
-			{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-			{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      {
+        test: /\.(woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          prefix: 'font/',
+          limit: '5000',
+        },
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'application/octet-stream',
+        },
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: '10000',
+          mimetype: 'image/svg+xml',
+        },
+      },
     ],
   },
 };
